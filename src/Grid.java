b299a5924b5edaf7580fb63b9ccf5178b31a630e
Duncan Mac Leod <duncan.macleod@berkeley.edu>
2020-05-26 13:25:23 -0700

    added win checking.

--- src/Grid.java
+++ src/Grid.java
@@ -1,5 +1,6 @@
+
 public class Grid {
-    Piece[][] positions;
+    Piece[][] board;
 
     enum Piece {
         RED,
@@ -8,12 +9,20 @@ public class Grid {
     }
 
     public Grid () {
-        positions = new Piece[6][7];
-        for (int i = 0; i < positions.length; i++) {
-            for (int j = 0; j < positions.length; j++) {
-                positions[i][j] = Piece.EMPTY;
+        board = new Piece[6][7];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = Piece.EMPTY;
             }
         }
+    }
+
+    public int getHeight() {
+        return board.length;
+    }
+
+    public int getWidth() {
+        return board[0].length;
     }
 
     /**
@@ -21,10 +30,45 @@ public class Grid {
      * @return success or not
      */
     public boolean addPiece (int column, Piece piece) {
-        for (int i = positions.length - 1; i >= 0; i--) {
-            if (positions[i][column].equals(Piece.EMPTY)) {
-                positions[i][column] = piece;
+        for (int i = board.length - 1; i >= 0; i--) {
+            if (board[i][column].equals(Piece.EMPTY)) {
+                board[i][column] = piece;
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean wonGame (Piece turn) {
+
+        // horizontalCheck
+        for (int j = 0; j < getWidth() - 3; j++) {
+            for (int i = 0; i < getHeight(); i++){
+                if (this.board[i][j] == turn && this.board[i][j+1] == turn && this.board[i][j+2] == turn && this.board[i][j+3] == turn){
+                    return true;
+                }
+            }
+        }
+        // verticalCheck
+        for (int i = 0; i < this.getHeight() - 3; i++){
+            for (int j = 0; j < getWidth(); j++){
+                if (this.board[i][j] == turn && this.board[i+1][j] == turn && this.board[i+2][j] == turn && this.board[i+3][j] == turn){
+                    return true;
+                }
+            }
+        }
+        // ascendingDiagonalCheck
+        for (int j = 0; j < getWidth()-3; j++){
+            for (int i = 0; i < getHeight()-3; i++){
+                if (this.board[i][j] == turn && this.board[i+1][j+1] == turn && this.board[i+2][j+2] == turn && this.board[i+3][j+3] == turn)
+                    return true;
+            }
+        }
+        // descendingDiagonalCheck
+        for (int j = 0; j < getWidth()-3; j++){
+            for (int i = getHeight() - 1; i > 2; i--){
+                if (this.board[i][j] == turn && this.board[i-1][j+1] == turn && this.board[i-2][j+2] == turn && this.board[i-3][j+3] == turn)
+                    return true;
             }
         }
         return false;
@@ -33,8 +77,8 @@ public class Grid {
     @Override
     public String toString() {
         String grid = "";
-        for (int i = 0; i < positions.length; i++) {
-            for (Piece piece : positions[i]) {
+        for (int i = 0; i < board.length; i++) {
+            for (Piece piece : board[i]) {
                 if (piece == Piece.RED) {
                     grid += "R ";
                 } else if (piece == Piece.YELLOW) {
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 	    Grid grid = new Grid();
-	    grid.addPiece(0, Grid.Piece.RED);
-	    grid.addPiece(0, Grid.Piece.RED);
-	    grid.addPiece(3, Grid.Piece.YELLOW);
         System.out.println(grid);
+        System.out.println(grid.wonGame(Grid.Piece.RED));
+        grid.addPiece(0, Grid.Piece.RED);
+        System.out.println(grid);
+        System.out.println(grid.wonGame(Grid.Piece.RED));
+        grid.addPiece(0, Grid.Piece.RED);
+        grid.addPiece(0, Grid.Piece.RED);
+        grid.addPiece(0, Grid.Piece.RED);
+        System.out.println(grid);
+        System.out.println(grid.wonGame(Grid.Piece.RED));
     }
 }
